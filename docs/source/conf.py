76cf9dcb045fe710d0332a6fd132afe5e16d2ce4
Flop-py <65293477+Flop-py@users.noreply.github.com>
2020-05-19 23:47:33 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath('../../relaxed_lasso/'))
 sys.path.append(os.path.join(os.path.dirname(__name__), '../..'))
+autodoc_mock_imports = ["relaxed_lasso"]
 
 
 # -- Project information -----------------------------------------------------
