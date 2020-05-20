cc1a4d86a5acfcb47225754eb783d4e6a605e10c
Flop-py <65293477+Flop-py@users.noreply.github.com>
2020-05-20 15:10:51 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,15 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../relaxed_lasso/'))
-sys.path.append(os.path.join(os.path.dirname(__name__), '../..'))
-
-import mock
-
-MOCK_MODULES = ['numpy']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
-
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
